2d8636b27ee5769396ea4d5691ec6e67e42782d9
4ndr3s <andres30092000@hotmail.com>
2024-05-13 13:54:39 -0500

    Archivos nuevos

---
+++ Grabadora de Voz/grabadora.py
@@ -0,0 +1,21 @@
+import sounddevice as sd
+import scipy.signal as signal
+import numpy as np
+
+frecuencia_muestreo = 44100
+duracion = 5
+
+print("Grabando...")
+entrada_audio = sd.rec(int(duracion * frecuencia_muestreo), samplerate = frecuencia_muestreo, channels = 1, dtype = np.float32)
+sd.wait()
+
+frecuencia_corte_bajo = 4000
+b, a = signal.butter(4, frecuencia_corte_bajo/(frecuencia_muestreo/2), "low")
+filtered_audio = signal.lfilter(b, a, entrada_audio)
+
+print("Audio original...")
+sd.play(entrada_audio, samplerate = frecuencia_muestreo)
+sd.wait()
+
+print("Audio filtrado...")
+sd.play(filtered_audio, samplerate = frecuencia_muestreo)
